d6baa846cc274ef6761008a1684a6c84f50451a1
Daniela Farias <farias.danicris@gmail.com>
2022-04-26 11:20:51 -0300

    Conversão para TSX

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
-// https://api.github.com/users/danielafarias/repos
+
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 export function RepositoryList() {
 
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
   
   useEffect(() => {
     fetch('https://api.github.com/users/danielafarias/repos')
