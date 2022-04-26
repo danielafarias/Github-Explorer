4f1b4de12611d05e19148ea1ad9666869b282c6b
Daniela Farias <farias.danicris@gmail.com>
2022-04-26 08:55:36 -0300

    uso do useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
+import { useState, useEffect } from "react";
 import { RepositoryItem } from "./RepositoryItem";
-
+// https://api.github.com/users/danielafarias/repos
 const repository = {
   name: "repository",
   description: "Forms in React",
@@ -7,6 +8,15 @@ const repository = {
 };
 
 export function RepositoryList() {
+
+  const [repositories, setRepositories] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://api.github.com/users/danielafarias/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data));
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
